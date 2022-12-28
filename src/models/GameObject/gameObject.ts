fff98ebd4c52a3a71971248aea2b528f36cd7da9
khkmgch <kohei.kamiguchi@gmail.com>
2022-12-29 02:26:06 +0900

    feat: GameObject

---
+++ src/models/GameObject/gameObject.ts
@@ -0,0 +1,19 @@
+//ゲーム内の全てのオブジェクトの親クラス
+export class GameObject extends Phaser.Physics.Arcade
+  .Sprite {
+  constructor(
+    params: {
+      scene: Phaser.Scene;
+      x: number;
+      y: number;
+    },
+    spriteKey: string
+  ) {
+    super(params.scene, params.x, params.y, spriteKey);
+
+    //Sceneに追加
+    params.scene.add.existing(this);
+    //物理計算を可能にする
+    params.scene.physics.world.enable(this);
+  }
+}
