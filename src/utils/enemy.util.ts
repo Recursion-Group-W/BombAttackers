e7c0ddadf810cc673e2813520df578e65a884410
khkmgch <kohei.kamiguchi@gmail.com>
2022-12-29 02:33:11 +0900

    feat: enemy.util

--- src/utils/enemy.util.ts
+++ src/utils/enemy.util.ts
@@ -1,31 +1,54 @@
-//ランダムな整数を返すメソッド
-const getRandomInt = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+import { GameScene } from '../scenes/GameScene';
+import { Position } from '../types/position';
 
-//ランダムな偶数を返すメソッド
-const getRandomEven = (num1: number, num2: number) => {
-  //偶数の配列
-  const evens = new Array(num2 - num1);
-  let even = num1;
-  for (let i = 0; i < evens.length; i++) {
-    evens[i] = even;
-    even += 2;
+export class EnemyUtil {
+  //ランダムな整数を返すメソッド
+  static getRandomInt(min: number, max: number) {
+    return (
+      Math.floor(Math.random() * (max - min + 1)) + min
+    );
   }
-  const randomIndex = getRandomInt(0, evens.length);
-  return evens[randomIndex];
-};
 
-//ランダムな奇数を返すメソッド
-const getRandomOdd = (num1: number, num2: number) => {
-  //奇数の配列
-  const odds = new Array(num2 - num1);
-  let odd = num1;
-  for (let i = 0; i < odds.length; i++) {
-    odds[i] = odd;
-    odd += 2;
+  //ランダムな偶数を返すメソッド
+  static getRandomEven(num1: number, num2: number) {
+    //偶数の配列
+    const evens = new Array(num2 - num1);
+    let even = num1;
+    for (let i = 0; i < evens.length; i++) {
+      evens[i] = even;
+      even += 2;
+    }
+    const randomIndex = EnemyUtil.getRandomInt(
+      0,
+      evens.length
+    );
+    return evens[randomIndex];
   }
-  const randomIndex = getRandomInt(0, odds.length);
-  return odds[randomIndex];
-};
-export { getRandomInt, getRandomEven, getRandomOdd };
+
+  //ランダムな奇数を返すメソッド
+  static getRandomOdd(num1: number, num2: number) {
+    //奇数の配列
+    const odds = new Array(num2 - num1);
+    let odd = num1;
+    for (let i = 0; i < odds.length; i++) {
+      odds[i] = odd;
+      odd += 2;
+    }
+    const randomIndex = EnemyUtil.getRandomInt(
+      0,
+      odds.length
+    );
+    return odds[randomIndex];
+  }
+  static getRandomPosition(scene: GameScene): Position {
+    const x = EnemyUtil.getRandomInt(
+      GameScene.TileSize * 2,
+      scene.getWidth() - GameScene.TileSize * 2
+    );
+    const y = EnemyUtil.getRandomInt(
+      GameScene.TileSize * 2,
+      scene.getHeight() - GameScene.TileSize * 2
+    );
+    return { x: x, y: y };
+  }
+}
