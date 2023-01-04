062680f2f0baf463f5c8fd7814dac633e7f5404c
khkmgch <kohei.kamiguchi@gmail.com>
2023-01-05 00:38:56 +0900

    ゲームに関するソケット通信を行う名前空間ioNspGameを作成

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import { createServer } from 'http';
+import { Server } from 'socket.io';
+import IoGame from './socket/ioGame';
 
 const app = express();
 const httpServer = createServer(app);
 
 const PORT = 5000;
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: ['http://localhost:3005'],
+  },
+});
+
+//ゲームに関するソケット通信を行うためのNameSpace
+const ioNspGame = io.of('/game');
+const ioGame = new IoGame(ioNspGame);
 
 httpServer.listen(PORT, () => {
   console.log(`Server is runnning PORT:${PORT}`);
