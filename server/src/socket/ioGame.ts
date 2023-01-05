1972216ab45e75a99e1c2be49ea9efdac15f27e7
khkmgch <kohei.kamiguchi@gmail.com>
2023-01-05 18:19:25 +0900

    ioGame内でclientIdを送信する処理を実装

--- server/src/socket/ioGame.ts
+++ server/src/socket/ioGame.ts
@@ -1,11 +1,18 @@
 import { Namespace } from 'socket.io';
+import RoomManager from '../manager/roomManager';
 
 export default class IoGame {
   time: Date = new Date();
 
-  constructor(public ioNspGame: Namespace) {
+  constructor(public ioNspGame: Namespace, public roomManager: RoomManager) {
     ioNspGame.on('connect', async (socket: any) => {
       console.log('クライアントと接続しました');
+
+      //clientIdを生成して送信する処理
+      roomManager.generateClientId(socket)
+
+
+      
 
       //接続が切れたとき
       socket.on('disconnect', () => {
